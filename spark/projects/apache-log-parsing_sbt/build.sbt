84561cb4a3dcef1262cb006c6daf66b239558e8c
chettrimukendra7753 <64076137+chettrimukendra7753@users.noreply.github.com>
2020-05-03 21:28:37 +0530

    Log Parsing Project for Spark

--- spark/projects/apache-log-parsing_sbt/build.sbt
+++ spark/projects/apache-log-parsing_sbt/build.sbt
@@ -2,11 +2,18 @@ name := "Apache Log Parsing"
 
 version := "0.0.1"
 
-scalaVersion := "2.10.4"
+scalaVersion := "2.11.8"
+
+val sparkVersion = "2.1.1"
 
 // additional libraries
+resolvers ++= Seq(
+  "apache-snapshots" at "http://repository.apache.org/snapshots/"
+)
+
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "1.5.2" % "provided",
-  "com.holdenkarau" %% "spark-testing-base" % "1.5.2_0.6.0" % "test"
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
+  "com.holdenkarau"  %% "spark-testing-base" % "1.5.2_0.6.0" % "test"
   // "org.scalatest" % "scalatest_2.11" % "3.0.1" % "test",
 )
--- spark/projects/apache-log-parsing_sbt/src/main/scala/com/cloudxlab/logparsing/log-parser.scala
+++ spark/projects/apache-log-parsing_sbt/src/main/scala/com/cloudxlab/logparsing/log-parser.scala
@@ -1,64 +1,85 @@
 package com.cloudxlab.logparsing
 
 import org.apache.spark._
-import org.apache.spark.SparkContext
+import org.apache.spark.sql.types._
+import org.apache.spark.sql._
 import org.apache.spark.rdd.RDD
-import org.apache.spark.SparkContext._
 
-class Utils extends Serializable {
-    val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)(.*)" (\d{3}) (\S+)""".r
 
-    def containsIP(line:String):Boolean = return line matches "^([0-9\\.]+) .*$"
-    //Extract only IP
-    def extractIP(line:String):(String) = {
-        val pattern = "^([0-9\\.]+) .*$".r
-        val pattern(ip:String) = line
-        return (ip.toString)
-    }
-
-    def gettop10(accessLogs:RDD[String], sc:SparkContext, topn:Int):Array[(String,Int)] = {
-        //Keep only the lines which have IP
-        var ipaccesslogs = accessLogs.filter(containsIP)
-        var cleanips = ipaccesslogs.map(extractIP(_)).filter(isClassA)
-        var ips_tuples = cleanips.map((_,1));
-        var frequencies = ips_tuples.reduceByKey(_ + _);
-        var sortedfrequencies = frequencies.sortBy(x => x._2, false)
-        return sortedfrequencies.take(topn)
-    }
-    
-    def isClassA(ip:String):Boolean = {
-         ip.split('.')(0).toInt < 127
-    }
-    
-}
 
 object EntryPoint {
-    val usage = """
+
+val PATTERN = """^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(\S+) (\S+)(.*)" (\d{3}) (\S+)""".r
+val schemaString = "host timeStamp url httpCode bytesTransfered"
+
+
+val fieldsArray = schemaString.split(" ")
+val fields = fieldsArray.map(
+f => StructField(f, StringType, nullable = true)
+)
+val schema = StructType(fields)
+
+def parseLogLine(log: String) :(String,String,String,String,String) = {
+val res = PATTERN.findFirstMatchIn(log) 
+if (res.isEmpty){
+println("Rejected Log Line: " + log)
+("Empty", "", "",  "-1", "")
+} else {
+val m = res.get
+return (m.group(1).trim().toUpperCase(), m.group(4).trim().toUpperCase(),m.group(6).trim().toUpperCase(), m.group(8).trim().toUpperCase(), m.group(9).trim().toUpperCase())}
+}
+
+
+
+    def main(args: Array[String]) {
+	
+	    val usage = """
         Usage: EntryPoint <how_many> <file_or_directory_in_hdfs>
         Eample: EntryPoint 10 /data/spark/project/access/access.log.45.gz
     """
-    
-    def main(args: Array[String]) {
-        
-        if (args.length != 3) {
+	
+	        if (args.length != 3) {
             println("Expected:3 , Provided: " + args.length)
             println(usage)
             return;
         }
 
-        var utils = new Utils
-
         // Create a local StreamingContext with batch interval of 10 second
-        val conf = new SparkConf().setAppName("WordCount")
-        val sc = new SparkContext(conf);
-        sc.setLogLevel("WARN")
+		
+  val spark = SparkSession
+   .builder()
+   .appName("LogParsing")
+   .getOrCreate()
+   
+   
+//        val conf = new SparkConf().setAppName("LogParsing")
+//        val sc = new SparkContext(conf);
+   
+spark.sparkContext.setLogLevel("WARN")
 
-        // var accessLogs = sc.textFile("/data/spark/project/access/access.log.45.gz")
-        var accessLogs = sc.textFile(args(2))
-        val top10 = utils.gettop10(accessLogs, sc, args(1).toInt)
-        println("===== TOP 10 IP Addresses =====")
-        for(i <- top10){
-            println(i)
-        }
+val logFile = spark.sparkContext.textFile(args(2))
+
+val rowRDD = logFile.map(parseLogLine).filter(x=> x._1 != "Empty").map(x => Row(x._1,x._2,x._3,x._4,x._5)).persist()
+
+import spark.implicits._
+
+val nsaLogDF = spark.createDataFrame(rowRDD, schema)
+
+val nsaLogDF2 = nsaLogDF.selectExpr("host","timeStamp","url","cast(httpCode as int) httpCode","bytesTransfered")
+
+nsaLogDF2.createOrReplaceTempView("nasa_log")
+
+val output = spark.sql("select * from nasa_log")
+spark.sql("cache TABLE nasa_log")
+
+
+spark.sql("select url,count(*) as req_cnt from nasa_log where url like '%HTML%' group by url order by req_cnt desc LIMIT 10").show
+spark.sql("select url,count(*) as req_cnt from nasa_log where url like '%/KSC.HTML%' group by url").show
+spark.sql("select substr(timeStamp,1,14),count(*) as req_cnt from nasa_log group by substr(timeStamp,1,14) order by req_cnt desc LIMIT 5").show
+spark.sql("select substr(timeStamp,1,14),count(*) as req_cnt from nasa_log group by substr(timeStamp,1,14) order by req_cnt asc LIMIT 5").show
+spark.sql("select httpCode,count(*) as req_cnt from nasa_log group by httpCode ").show
+
+
+
     }
 }
